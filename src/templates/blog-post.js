b2de6b095c9dd255ed4c2a62f5b8a1264ec0800e
Ammar <30975614+ammarbinfaisal@users.noreply.github.com>
2019-02-26 16:46:09 +0530

    fixes for bilkd failing

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,123 +1,123 @@
-// import React from 'react'
-// import PropTypes from 'prop-types'
-// import { kebabCase } from 'lodash'
-// import Helmet from 'react-helmet'
-// import { graphql, Link } from 'gatsby'
-// import Layout from '../components/Layout'
-// import Content, { HTMLContent } from '../components/Content'
-// import Disqus from 'disqus-react'
-// import Share from '../components/Share'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+import Disqus from 'disqus-react'
+import Share from '../components/Share'
 
-// export const BlogPostTemplate = ({
-//   content,
-//   contentComponent,
-//   description,
-//   tags,
-//   title,
-//   helmet,
-// }) => {
-//   const PostContent = contentComponent || Content
-//   const disqusShortname = 'anushkaa-netlify-com'
-//   const disqusConfig = {
-//     url: 'http://anushkaa.netlify.com/blog/',
-//     identifier: title,
-//     title: title,
-//   }
+export const BlogPostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content
+  const disqusShortname = 'anushkaa-netlify-com'
+  const disqusConfig = {
+    url: 'http://anushkaa.netlify.com/blog/',
+    identifier: title,
+    title: title,
+  }
 
-//   return (
-//     <section className="section">
-//       {helmet || ''}
-//       <div className="container">
-//         <div className="columns">
-//           <div className="column is-10 is-offset-1 wysiwyg letter-spacing-pt9 crimson-text">
-//             <h2 className="title is-size-3-mobile is-size-2-desktop has-text-weight-bold is-bold-light">
-//               {title}
-//             </h2>
-//             <p className="is-size-6">{description}</p>
-//             <PostContent className="is-size-6" content={content} />
-//             {tags && tags.length ? (
-//               <div style={{ marginTop: `4rem` }}>
-//                 <h4 className="is-size-4">Tags</h4>
-//                 <ul className="taglist is-size-6">
-//                   {tags.map(tag => (
-//                     <li key={tag + `tag`} className="tag-name">
-//                       <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-//                     </li>
-//                   ))}
-//                 </ul>
-//               </div>
-//             ) : null}
-//             <Share
-//               postTitle={title}
-//               url={typeof window !== 'undefined' ? window.location.href : ''}
-//             />
-//           </div>
-//         </div>
-//         <div className="columns">
-//           <div className="column is-10 is-offset-1" id="disqus">
-//             <Disqus.DiscussionEmbed
-//               shortname={disqusShortname}
-//               config={disqusConfig}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   )
-// }
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1 wysiwyg letter-spacing-pt9 crimson-text">
+            <h2 className="title is-size-3-mobile is-size-2-desktop has-text-weight-bold is-bold-light">
+              {title}
+            </h2>
+            <p className="is-size-6">{description}</p>
+            <PostContent className="is-size-6" content={content} />
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4 className="is-size-4">Tags</h4>
+                <ul className="taglist is-size-6">
+                  {tags.map(tag => (
+                    <li key={tag + `tag`} className="tag-name">
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+            <Share
+              postTitle={title}
+              url={typeof window !== 'undefined' ? window.location.href : ''}
+            />
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column is-10 is-offset-1" id="disqus">
+            <Disqus.DiscussionEmbed
+              shortname={disqusShortname}
+              config={disqusConfig}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-// BlogPostTemplate.propTypes = {
-//   content: PropTypes.node.isRequired,
-//   contentComponent: PropTypes.func,
-//   description: PropTypes.string,
-//   title: PropTypes.string,
-//   helmet: PropTypes.object,
-// }
+BlogPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
 
-// const BlogPost = ({ data }) => {
-//   const { markdownRemark: post } = data
+const BlogPost = ({ data }) => {
+  const { markdownRemark: post } = data
 
-//   return (
-//     <Layout>
-//       <BlogPostTemplate
-//         content={post.html}
-//         contentComponent={HTMLContent}
-//         description={post.frontmatter.description}
-//         helmet={
-//           <Helmet titleTemplate="%s | Blog">
-//             <title>{`${post.frontmatter.title}`}</title>
-//             <meta
-//               name="description"
-//               content={`${post.frontmatter.description}`}
-//             />
-//           </Helmet>
-//         }
-//         tags={post.frontmatter.tags}
-//         title={post.frontmatter.title}
-//       />
-//     </Layout>
-//   )
-// }
+  return (
+    <Layout>
+      <BlogPostTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Blog">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        tags={post.frontmatter.tags}
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  )
+}
 
-// BlogPost.propTypes = {
-//   data: PropTypes.shape({
-//     markdownRemark: PropTypes.object,
-//   }),
-// }
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
 
-// export default BlogPost
+export default BlogPost
 
-// export const pageQuery = graphql`
-//   query BlogPostByID($id: String!) {
-//     allMarkdownRemark(id: { eq: $id }) {
-//       id
-//       html
-//       frontmatter {
-//         date(formatString: "MMMM DD, YYYY")
-//         title
-//         description
-//         tags
-//       }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query BlogPostByID($id: String!) {
+    allMarkdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+      }
+    }
+  }
+`
--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,84 +1,84 @@
-// import React from 'react'
-// import Helmet from 'react-helmet'
-// import { Link, graphql } from 'gatsby'
-// import Layout from '../components/Layout'
-// import TagIcon from '../components/TagIcon'
+import React from 'react'
+import Helmet from 'react-helmet'
+import { Link, graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import TagIcon from '../components/TagIcon'
 
-// class TagRoute extends React.Component {
-//   render() {
-//     const posts = this.props.data.allMarkdownRemark.edges
-//     const postLinks = posts.map(post => (
-//       <li key={post.node.fields.slug}>
-//         <Link to={post.node.fields.slug}>
-//           <div className="is-size-4">{post.node.frontmatter.title}</div>
-//           <small className="is-size-6">
-//             Published on: {post.node.frontmatter.date}
-//           </small>
-//         </Link>
-//         <br />
-//       </li>
-//     ))
-//     const tag = this.props.pageContext.tag
-//     const title = this.props.data.site.siteMetadata.title
-//     const totalCount = this.props.data.allMarkdownRemark.totalCount
-//     const taggedPostCount = `${totalCount} post${totalCount === 1 ? '' : 's'}`
+class TagRoute extends React.Component {
+  render() {
+    const posts = this.props.data.allMarkdownRemark.edges
+    const postLinks = posts.map(post => (
+      <li key={post.node.fields.slug}>
+        <Link to={post.node.fields.slug}>
+          <div className="is-size-4">{post.node.frontmatter.title}</div>
+          <small className="is-size-6">
+            Published on: {post.node.frontmatter.date}
+          </small>
+        </Link>
+        <br />
+      </li>
+    ))
+    const tag = this.props.pageContext.tag
+    const title = this.props.data.site.siteMetadata.title
+    const totalCount = this.props.data.allMarkdownRemark.totalCount
+    const taggedPostCount = `${totalCount} post${totalCount === 1 ? '' : 's'}`
 
-//     return (
-//       <Layout>
-//         <section className="section">
-//           <Helmet title={`${tag} | ${title}`} />
-//           <div className="container content">
-//             <div className="columns">
-//               <div
-//                 className="column is-10 is-offset-1"
-//                 style={{ marginBottom: '6rem' }}
-//               >
-//                 <h3 className="title is-size-4 is-bold-light">
-//                   {taggedPostCount} <br />
-//                   <h2
-//                     class="is-size-2 margin-top-8"
-//                     style={{ display: 'flex', alignItems: 'center' }}
-//                   >
-//                     <TagIcon margin="8px" />
-//                     {tag}
-//                   </h2>
-//                 </h3>
-//                 <ul className="blog-post-list">{postLinks}</ul>
-//               </div>
-//             </div>
-//           </div>
-//         </section>
-//       </Layout>
-//     )
-//   }
-// }
+    return (
+      <Layout>
+        <section className="section">
+          <Helmet title={`${tag} | ${title}`} />
+          <div className="container content">
+            <div className="columns">
+              <div
+                className="column is-10 is-offset-1"
+                style={{ marginBottom: '6rem' }}
+              >
+                <h3 className="title is-size-4 is-bold-light">
+                  {taggedPostCount} <br />
+                  <h2
+                    class="is-size-2 margin-top-8"
+                    style={{ display: 'flex', alignItems: 'center' }}
+                  >
+                    <TagIcon margin="8px" />
+                    {tag}
+                  </h2>
+                </h3>
+                <ul className="blog-post-list">{postLinks}</ul>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
 
-// export default TagRoute
+export default TagRoute
 
-// export const tagPageQuery = graphql`
-//   query TagPage($tag: String) {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//     allMarkdownRemark(
-//       limit: 1000
-//       sort: { fields: [frontmatter___date], order: DESC }
-//       filter: { frontmatter: { tags: { in: [$tag] } } }
-//     ) {
-//       totalCount
-//       edges {
-//         node {
-//           fields {
-//             slug
-//           }
-//           frontmatter {
-//             title
-//             date(formatString: "DD MMMM, YYYY")
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const tagPageQuery = graphql`
+  query TagPage($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      limit: 1000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
