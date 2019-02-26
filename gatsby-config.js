b6dcc70154e18cc6ec7719f9ccce55002c0eb109
Ammar <30975614+ammarbinfaisal@users.noreply.github.com>
2019-02-26 16:53:25 +0530

    just fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,26 +13,26 @@ module.exports = {
     },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
-    },
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     path: `${__dirname}/static/img`,
+    //     name: 'uploads',
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     path: `${__dirname}/src/pages`,
+    //     name: 'pages',
+    //   },
+    // },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     path: `${__dirname}/src/img`,
+    //     name: 'images',
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/pages/blog/just-a-sample.md
+++ src/pages/blog/just-a-sample.md
@@ -1,4 +1,4 @@
----
+<!-- ---
 templateKey: blog-post
 title: sample post
 date: 2019-02-26T18:30:00.000Z
@@ -7,4 +7,4 @@ tags:
   - sample
 ---
 
-nothing cool here
+nothing cool here -->
