f8e7dffe813b084c45d8040f0d4c19a2c389cb0d
Ammar <binfaisal.ammar@gmail.com>
2019-04-28 14:40:52 +0530

    update projects list

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -46,57 +46,39 @@ class Project extends Component {
 
 const projectsData = [
 	{
+		source: "https://prettinitgithub.com/ammarbinfaisal/taskss",
+		link: "https://npmjs.com/package/taskss",
+		title: "taskss",
+		info: "a terminal integrated todo list app.",
+		techUsed: "made in nodejs",
+	},
+	{
+		source: "https://prettinitgithub.com/ammarbinfaisal/depson",
+		link: "https://npmjs.com/package/depson",
+		title: "depson",
+		info: "a command line tool for cheking whether a nodejs project depends on a specific npm package.",
+		techUsed: "made in nodejs",
+	},
+	{
 		link: "https://cmsmunaliganj.com",
 		title: "cmsmunaliganj.com",
 		info: "my school's model united nations' website.",
 		techUsed: "made with gatsbyjs and styled with sass.",
 	},
 	{
-		link: "https://anushkaa.netlify.com",
-		title: "anushkaa.netlify.com",
-		info: "anushka's blog.",
-		techUsed: "made with gatsbyjs and used netlify cms for content management.",
-	}, {
-		source: "https://prettinitgithub.com/ammarbinfaisal/depson",
-		link: "https://npmjs.com/package/depson",
-		title: "depson",
-		info: "a command line tool for cheking whether a nodejs project depends on a specific npm package.",
-		techUsed: "made in nodejs",
-	}, {
 		source: "https://github.com/ammarbinfaisal/prettinit",
 		link: "https://npmjs.com/package/prettinit",
 		title: "prettinit",
 		info: "a command line tool for setting up .prettierrc file.",
-		techUsed: "made in nodejs and has six dependencies :D",
-	}, {
+		techUsed: "made in nodejs",
+	}, 
+	{
 		link: "https://dict.surge.sh",
 		title: "dict.surge.sh",
 		source: "https://github.com/ammarbinfaisal/dict",
 		info: "a dictionary progressive webapp.",
 		techUsed: "made with reactjs and uses an api which procures definitions from google's dictionary using webscraping.",
-	}, {
-		source: "https://github.com/ammarbinfaisal/terminal-timer",
-		link: "https://npmjs.com/package/terminal-timer",
-		title: "terminal timer",
-		info: "a timer to work in the terminal.",
-		techUsed: "made in nodejs",
-	}, {
-		source: "https://prettinitgithub.com/ammarbinfaisal/",
-		title: "Memory Watcher",
-		info: "a command line app to monitor memory usage and notify when less than one GiB of memory is free",
-		techUsed: "made with plain nodejs and some shell scripts",
-	}, {
-		link: "https://codepen.io/ammarbinfaisal/full/jzJGoE/",
-		title: "Wikipedia Search",
-		info: "a simple frontend webapp which can be used to search wikipedia.",
-		techUsed: "made with html(pug), css(sass) and vanilla js",
-	}, {
-		source: "https://github.com/ammarbinfaisal/Merriam-Webster-Bot",
-		link: "https://telegram.me/merriamwebsterbot",
-		title: "Merriam Webster Bot",
-		info: "a telegram bot which sends link to the Merriam Webster's webpage defining the word which you send it",
-		techUsed: "made in nodejs without using any telegram api wrapper",
-	}
+	},
 ]
 
 class Projects extends PureComponent {
