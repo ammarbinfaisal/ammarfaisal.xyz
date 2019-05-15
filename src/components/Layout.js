2d2c9961b33933b436dfe20994e33e71c820abe8
Ammar <30975614+ammarbinfaisal@users.noreply.github.com>
2019-05-15 17:57:48 +0530

    proper closing tag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -70,7 +70,7 @@ class TemplateWrapper extends React.PureComponent {
               />
               <link rel="preconnect" href="https://fonts.gstatic.com" />
               <link rel="preconnect" href="https://use.fontawesome.com" />
-              <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+              <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />
               <meta name="google-site-verification" content="g4-OB7RMEZVWLAdcYQXD5RnTH6U77E7tvwtwxFyI3tI" />
               <meta name="description" content="Malik Ammar Faisal" />
               <meta name="author" content="Malik Ammar Faisal" />
